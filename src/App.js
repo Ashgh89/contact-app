0616e485e2e3a6d89cbc76cef2488335aca83048
Iman Ghanei <ghaneiash@gmail.com>
2022-04-22 11:39:44 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import "./App.css";
 import AddContact from "./component/AddContact/AddContact";
 
 function App() {
-  const [contacts, setContact] = useState([]);
+  const [contacts, setContacts] = useState([]);
 
-  const addContactHandler = (e) => {
-    e.preventDefault();
-    console.log("clicked");
+  const addContactHandler = (contact) => {
+    // console.log(contact); // So it works, now let's do it
+    // ceil make the number rond
+    setContacts([
+      ...contacts,
+      { id: Math.ceil(Math.random() * 100), ...contact },
+    ]);
+    // ---OR--- I prefer up there
+    // const newAdded = {
+    //   id: Math.ceil(Math.random() * 100),
+    //   name: contact.name,
+    //   email: contact.email,
+    // };
+    // setContacts([...contacts, newAdded]);
   };
 
   return (
