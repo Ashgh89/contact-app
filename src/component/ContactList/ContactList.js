5aa238c0b0196b885a4b2b858689866583026f60
Iman Ghanei <ghaneiash@gmail.com>
2022-04-23 09:48:28 +0200

    changes

--- src/component/ContactList/ContactList.js
+++ src/component/ContactList/ContactList.js
@@ -1,17 +1,21 @@
+import styles from "./contactList.module.css";
+
 const ContactList = ({ contacts, onDelete }) => {
   return (
-    <>
+    <section className={styles.contactList}>
       {contacts.map((contact) => {
         const { name, email, id } = contact;
         return (
-          <div key={id}>
-            <p>name:{name}</p>
-            <p>email:{email}</p>
+          <div key={id} className={styles.item}>
+            <div>
+              <p>name: {name}</p>
+              <p>email: {email}</p>
+            </div>
             <button onClick={() => onDelete(id)}>delete</button>
           </div>
         );
       })}
-    </>
+    </section>
   );
 };
 export default ContactList;
