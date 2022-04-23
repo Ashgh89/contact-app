e369125711a9b88460cc067d87f7cb99d8d9d057
Iman Ghanei <ghaneiash@gmail.com>
2022-04-23 13:35:44 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddContact from "./component/AddContact/AddContact";
 import ContactList from "./component/ContactList/ContactList";
@@ -27,6 +27,27 @@ function App() {
     const filteredContact = contacts.filter((c) => c.id !== id);
     setContacts(filteredContact);
   };
+
+  //localStorage
+  // CDM -> get data
+  // contacts -> save
+  useEffect(() => {
+    //JSON parsing is the process of converting a JSON object in text format to a Javascript object
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+
+    if (savedContacts) setContacts(savedContacts);
+  }, []);
+
+  // localStorage.setItem(key (must be string),value (string));
+  //Convert a JavaScript object into a string with JSON.stringify().
+  //When sending data to a web server, the data has to be a string.
+  useEffect(() => {
+    // this line of code doesn't work, i dunno why
+    // localStorage.setItem("contacts", JSON.stringify(contacts));
+    if (contacts.length !== 0) {
+      localStorage.setItem("contacts", JSON.stringify(contacts));
+    }
+  }, [contacts]);
 
   return (
     <main className="App">
