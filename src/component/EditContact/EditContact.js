485caa735d08a74ad7ecc09b4c3f02c9b92c7fad
Iman Ghanei <ghaneiash@gmail.com>
2022-04-29 12:59:40 +0200

    changes

---
+++ src/component/EditContact/EditContact.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./addContact.module.css";
+const AddContact = ({ addContactHandler }) => {
+  const [contact, setContact] = useState({ name: "", email: "" });
+  const navigate = useNavigate();
+  const changeHandler = (e) => {
+    // When our state is an object, we make a copy (...contact)
+    // { ...contact, [e.target.name]: e.target.value } -> With this you can use
+    // just one changeHandler for multiple inputs
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+  const submitForm = (e) => {
+    if (!contact.name || !contact.email) {
+      alert("Please Enter name or email");
+      return;
+    }
+    e.preventDefault();
+    addContactHandler(contact);
+    setContact({ name: "", email: "" });
+
+    // push to homepage
+    navigate("/");
+  };
+  return (
+    <form onSubmit={submitForm}>
+      <div className={styles.formControl}>
+        <label>Name</label>
+        <input
+          type="text"
+          // [e.target.name]: up there, is relevant with that one here (name)
+          // and its value name=("name") should be equal to that one in useState
+          name="name"
+          value={contact.name}
+          onChange={changeHandler}
+        />
+      </div>
+      <div className={styles.formControl}>
+        <label>Email</label>
+        <input
+          type="text"
+          name="email"
+          value={contact.email}
+          onChange={changeHandler}
+        />
+      </div>
+      <button className={styles.btn_style} type="submit">
+        Add Contact
+      </button>
+    </form>
+  );
+};
+export default AddContact;
